d5bfc80352682d0e354b3fd287e0dff4e925ac31
Ashini Nanayakkara <ashinireshana@gmail.com>
2024-04-08 03:37:31 +0530

    home edited

--- NSBMGO/Home.cs
+++ NSBMGO/Home.cs
@@ -18,6 +18,31 @@ namespace NSBMGO
             InitializeComponent();
         }
 
-        public bool TopLevel {  get; internal set; } 
+        public bool TopLevel {  get; internal set; }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label31_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
