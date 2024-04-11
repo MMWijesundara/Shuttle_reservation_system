2428ea0eb66b5e66d0ea714c0df8f185a7978852
Meth Wijesundara <methmihisara@gmail.com>
2024-04-11 13:40:40 +0530

    driver user control size changed

--- NSBMGO/Reserve.Designer.cs
+++ NSBMGO/Reserve.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Reserve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Reserve";
+            this.Size = new System.Drawing.Size(1102, 593);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
