eec888ae5055a9a5bb0765c531143646ae34d6df
Hasara Randunu <hasara.randunu@gmail.com>
2024-04-12 15:16:30 +0530

    Frame 1

---
+++ NSBMGO/Frame 1 seats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NSBMGO
+{
+    public partial class Frame_1_seats : UserControl
+    {
+        public Frame_1_seats()
+        {
+            InitializeComponent();
+        }
+    }
+}
