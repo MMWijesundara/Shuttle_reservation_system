9783f01b89695731baae937e5400928ecb64cf4d
Meth Wijesundara <methmihisara@gmail.com>
2024-05-06 20:11:33 +0530

    driver update issue fix. (no need of conversion)

--- NSBMGO/Program.cs
+++ NSBMGO/Program.cs
@@ -18,7 +18,7 @@ namespace NSBMGO
             Application.SetCompatibleTextRenderingDefault(false);
 
 
-            Application.Run(new Loading_screen());
+            Application.Run(new Operator_dashboard());
 
 
         }
